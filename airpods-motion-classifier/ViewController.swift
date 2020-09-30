6a5fc57fe9cfae249e5db42a8b0ef39e930c9e88
yorifuji <yorifuji@gmail.com>
2020-09-30 15:51:46 +0900

    Initial Commit

---
+++ airpods-motion-classifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  airpods-motion-classifier
+//
+//  Created by yorifuji on 2020/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
